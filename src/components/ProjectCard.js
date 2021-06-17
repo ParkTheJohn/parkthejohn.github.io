3a0139f483e9a992d8a8c6074714a5ccf1832d8d
Jongwoo Park <39757869+ParkTheJohn@users.noreply.github.com>
2021-06-17 15:09:17 -0700

    Added dialog
    Added dialog to project card on click

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-// import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-// import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import projectList from "./ProjectsData.js";
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles({
   root: {
@@ -19,27 +24,58 @@ const useStyles = makeStyles({
 });
 
 const ProjectCard = (param) => {
-
+    const [open, setOpen] = React.useState(false);
+    const theme = useTheme();
+    const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
     const classes = useStyles();
 
+    const handleClickOpen = () => {
+        setOpen(true);
+      };
+    
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return (
-        <Card className={classes.root}>
-            <CardActionArea>
-                <CardMedia
-                className={classes.media}
-                image={projectList[param.index].image}
-                title="Contemplative Reptile"
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        {projectList[param.index].name}
+        <div>
+            <Card className={classes.root}>
+                <CardActionArea
+                    onClick={handleClickOpen}
+                >
+                    <CardMedia
+                    className={classes.media}
+                    image={projectList[param.index].image}
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            {projectList[param.index].name}
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            {projectList[param.index].shortDescription}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+                <Dialog
+                    fullScreen={fullScreen}
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="responsive-dialog-title"
+                >
+                <DialogTitle id="responsive-dialog-title">{projectList[param.index].name}</DialogTitle>
+                <DialogContent dividers>
+                    <Typography >
+                            {projectList[param.index].shortDescription}
                     </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        {projectList[param.index].shortDescription}
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-        </Card>
+                </DialogContent>
+                <DialogActions>
+                    <Button autoFocus onClick={handleClose} >
+                        Close
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
     )
 }
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,32 +5,21 @@ import ProjectCard from "./ProjectCard";
 
 const Projects = () => {
 
+    const indices = [0, 1, 2, 3];
+
     return (
         <div className="projectsContainer">
             <div className="projectsTitleContainer">
                 <h1 className="projectsTitle">Projects.</h1>
             </div>
             <Grid container spacing={0}>
-                <Grid item xs={12} sm={6}>
+                {indices.map((index) => (
+                    <Grid key={index} item xs={12} sm={6}>
                     <div className="projectsContent">
-                        <ProjectCard index="0"/>
+                        <ProjectCard index={index}/>
                     </div>
                 </Grid>
-                <Grid item xs={12} sm={6}>
-                    <div className="projectsContent">
-                        <ProjectCard index="1"/>
-                    </div>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <div className="projectsContent">
-                        <ProjectCard index="2"/>
-                    </div>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <div className="projectsContent">
-                        <ProjectCard index="3"/>
-                    </div>
-                </Grid>
+                ))}
             </Grid>
         </div>
     )
